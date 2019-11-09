2e92f46763cd0131bbc2f434f2081615eb68729d
devops <ms.pnriyaz@gmail.com>
2019-11-09 21:07:24 +0530

    modified java

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World from dev2"); 
     } 
 } 
 
